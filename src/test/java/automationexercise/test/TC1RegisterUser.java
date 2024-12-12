1208b46c44bf1294416943ba8ff19d2d3b73fa69
MohamedAbdelrehem <mohamedabdelrehem32@gmail.com>
2024-12-13 00:36:35 +0200

    Add PropertiesReader utility and execution properties file; update test class to use dynamic browser configuration

--- src/test/java/automationexercise/test/TC1RegisterUser.java
+++ src/test/java/automationexercise/test/TC1RegisterUser.java
@@ -1,20 +1,22 @@
 package automationexercise.test;
 
+import automationexercise.pages.*;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
-import utilities.DriverFactory;
-import utilities.JsonFileManager;
-import utilities.ScreenshotHeadway;
-import automationexercise.pages.*;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utilities.DriverFactory;
+import utilities.JsonFileManager;
+import utilities.PropertiesReader;
+import utilities.ScreenshotHeadway;
 
 public class TC1RegisterUser {
 
-   private WebDriver driver;
+    private WebDriver driver;
     ScreenshotHeadway screenshot;
 
 
@@ -27,16 +29,20 @@ public class TC1RegisterUser {
     JsonFileManager testData;
 
 
-
     //* Setup Methods
     @BeforeClass
-    private void setup() {
+    private void loggingProperties() {
+        PropertiesReader.loadProperties();
+    }
 
-        driver = DriverFactory.initDriver("chrome", true);
+    @BeforeMethod
+    private void setup() {
+        driver = DriverFactory.initDriver(System.getProperty("browser"), true);
         testData = new JsonFileManager("D:/Headway2024/Automation/JAVA/HeadwayAutomationDemo/src/test/TestData/UsersTestData.json");
         // Our own class that takes the driver as an argument and makes taking screenshots easier
         screenshot = new ScreenshotHeadway(driver);
     }
+
 
     @AfterClass
     private void tearDown() {
@@ -48,16 +54,16 @@ public class TC1RegisterUser {
     @Description("Verify the user can successfully register, log in, and delete their account on the Automation Exercise website.")
     private void RegisterUser() {
         new HomePage(driver).navigateToHomePage()
-                            .assertHomePage();
+                .assertHomePage();
         new Navbar(driver).clickSignupLoginButton();
         new SignupLoginPage(driver).assertLoginSignUp()
-                                 .signUp(testData.getTestData("firstName"), testData.getTestData("lastName"), testData.getTestData("email"))
-                                .clickSignUpButton();
+                .signUp(testData.getTestData("firstName"), testData.getTestData("lastName"), testData.getTestData("email"))
+                .clickSignUpButton();
         new SignupFormPage(driver).assertSignUpPage()
-                        .fillRegisterForm(testData.getTestData("firstName"), testData.getTestData("lastName"), testData.getTestData("password"), testData.getTestData("day"), testData.getTestData("month"), testData.getTestData("year"), testData.getTestData("company"), testData.getTestData("address1"), testData.getTestData("address2"), testData.getTestData("country"), testData.getTestData("state"), testData.getTestData("city"), testData.getTestData("zipCode"), testData.getTestData("mobileNumber"))
-                        .clickCreateAccount();
+                .fillRegisterForm(testData.getTestData("firstName"), testData.getTestData("lastName"), testData.getTestData("password"), testData.getTestData("day"), testData.getTestData("month"), testData.getTestData("year"), testData.getTestData("company"), testData.getTestData("address1"), testData.getTestData("address2"), testData.getTestData("country"), testData.getTestData("state"), testData.getTestData("city"), testData.getTestData("zipCode"), testData.getTestData("mobileNumber"))
+                .clickCreateAccount();
         new ConfitmationAccountPage(driver).assertAccountCreated()
-                                .clickContinueButton();
+                .clickContinueButton();
         //? is it right to recreate the Navbar and ConfitmationAccountPage again
         new Navbar(driver).assertUserLoggedName("MohamedAbdelrehem")
                 .deleteAccount();
